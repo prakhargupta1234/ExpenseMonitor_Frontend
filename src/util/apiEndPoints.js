342cd8853518342be417cadba1eb65f3fcda2f2c
Prakhar Gupta <111242674+prakhargupta1234@users.noreply.github.com>
2026-05-03 02:11:49 +0530

    update the web page 030/05/20269

--- src/util/apiEndPoints.js
+++ src/util/apiEndPoints.js
@@ -1,5 +1,6 @@
-export const BASE_URL = "http://localhost:8080/api/v1.0";
-// export const BASE_URL = "https://expensemonitor-backend.onrender.com/api/v1.0";
+//export const BASE_URL = "http://localhost:8080/api/v1.0";
+export const BASE_URL = "https://expensemonitor-backend.onrender.com/api/v1.0";
+
 const CLOUDINARY_CLOUD_NAME = "dncoknrf7";
 
 export const API_ENDPOINTS = {
